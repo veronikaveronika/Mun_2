5ea35dbd6daf189f55451decebb18e303a64a7a2
veronika_t <splot2023@mail.ru>
2025-05-23 04:59:06 +0300

    Initial commit

---
+++ src/test/java/org/example/SimpleMunchenTest.java
@@ -0,0 +1,16 @@
+package com.juneit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleMunchenTest {
+
+    @Test
+    public void testOpenMunchenSite() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.muenchen.de");
+        driver.quit();
+    }
+}
